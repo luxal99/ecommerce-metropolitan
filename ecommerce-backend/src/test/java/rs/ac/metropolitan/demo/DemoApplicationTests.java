cd3fea8b43197d40d90ad5c0bfe40b83acf29606
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2020-12-12 13:40:17 +0100

    Init backend

---
+++ ecommerce-backend/src/test/java/rs/ac/metropolitan/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.metropolitan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
