cbf86e09e66dd725b8bf790de49a67d4b2733c45
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2020-12-18 12:06:27 +0100

    Generic and product service

---
+++ ecommerce-frontend/src/app/constant/const.ts
@@ -0,0 +1,11 @@
+export const PRODUCT_CATEGORY_ROUTE = "/productCategory";
+export const PRODUCT_BRAND_ROUTE = "/productBrand";
+export const PRODUCT_ROUTE = "/product";
+export const ROLE_ROUTE = "/role";
+export const PRODUCT_IMAGE_ROUTE = "/productImage";
+export const SHIPPING_ADDRESS_ROUTE = "/shippingAddress";
+export const ORDER_ROUTE = "/order";
+export const USER_INFO_ROUTE = "/userInfo";
+export const USER_ROUTE = "/user";
+export const TOKEN_NAME = "auth_token"
+export const TOKEN_PREFIX = "Bearer ";
---
+++ ecommerce-frontend/src/app/service/product.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+import {GenericService} from "./generic.service";
+import {Product} from "../models/Product";
+import {HttpClient} from "@angular/common/http";
+import {PRODUCT_ROUTE} from "../constant/const";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService extends GenericService<Product> {
+
+  constructor(http: HttpClient, router: string) {
+    super(http, PRODUCT_ROUTE);
+  }
+}
