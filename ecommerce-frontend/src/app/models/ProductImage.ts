c673a65108a90a7d1b0baaa526d9e736983be2ea
Aleksa <aleksa.lukic.3659@metropolitan.ac.rs>
2020-12-21 15:07:28 +0100

    Set product image service

---
+++ ecommerce-frontend/src/app/models/ProductImage.ts
@@ -0,0 +1,7 @@
+export class ProductImage {
+  id: number;
+  title: string;
+  url: string;
+
+
+}
--- ecommerce-frontend/src/app/service/product-image.service.ts
+++ ecommerce-frontend/src/app/service/product-image.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {GenericService} from './generic.service';
+import {ProductImage} from '../models/ProductImage';
+import {PRODUCT_IMAGE_ROUTE} from '../constant/const';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductImageService {
-
-  constructor() { }
+export class ProductImageService extends GenericService<ProductImage> {
+  route = PRODUCT_IMAGE_ROUTE;
 }
